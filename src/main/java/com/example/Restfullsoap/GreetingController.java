905d4a511864d08ab39b9554728bdd7d2085500d
Anas <anasomarzaitoun@gmail.com>
2020-06-11 15:00:23 +0300

    Added some changes

--- src/main/java/com/example/Restfullsoap/GreetingController.java
+++ src/main/java/com/example/Restfullsoap/GreetingController.java
@@ -16,4 +16,11 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
+    @GetMapping("/help")
+    public String help(){
+        return "This is a toy project, it's built to prove the idea that I understood the concepts " +
+                "of multiple technologies, you can send your try to request the link:" +
+                " http://localhost:8080/greeting?name=Hadi\n Additionally, you can writing the following command inside the project's directory:" +
+                "curl --header \"content-type: text/xml\" -d @request.xml http://localhost:8080/ws";
+    }
 }
