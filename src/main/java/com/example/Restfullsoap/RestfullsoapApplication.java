f1e621818279e979d692fe9fddf8bc2f2e3ff9ea
Anas <anasomarzaitoun@gmail.com>
2020-06-11 12:18:11 +0300

    initial commit

---
+++ src/main/java/com/example/Restfullsoap/RestfullsoapApplication.java
@@ -0,0 +1,14 @@
+package com.example.Restfullsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullsoapApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RestfullsoapApplication.class, args);
+	}
+
+}
